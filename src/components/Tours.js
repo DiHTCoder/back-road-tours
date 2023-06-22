507072d046b81b9d889c696fea6d61876a7180de
dihuynh@outlook.sg <dihuynh@outlook.sg>
2023-06-22 21:01:32 +0700

    split into component

---
+++ src/components/Tours.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Tittle from "./Tittle";
+import { tours } from "../data";
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Tittle title="featureed" subTitle="tour"></Tittle>
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <article className="tour-card" id={tour.id}>
+              <div className="tour-img-container">
+                <img src={tour.image} className="tour-img" alt="" />
+                <p className="tour-date">{tour.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{tour.title}</h4>
+                </div>
+                <p>{tour.info}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {tour.location}
+                  </p>
+                  <p>{tour.duration}</p>
+                  <p>{tour.cost}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
